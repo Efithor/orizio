eb6733aae490ba5480bbddb5e817a8f874439518
Kyle Connor <Efithor@gmail.com>
2017-11-03 15:25:03 -0500

    Is this allowed

--- index.js
+++ index.js
@@ -14,17 +14,11 @@ io.on('connection', function(socket){
   console.log('a user connected');
   socket.on('disconnect', function(){
     console.log('user disconnected');
-  });
-});
-
-io.on('connection', function(socket){
+  })
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
-  });
-});
-
-io.on('connection', function(socket){
+  })
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
-  });
+  })
 });
