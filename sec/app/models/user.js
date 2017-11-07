2122f4dcde966d3b16976de54c1eda22a044a9b5
Kyle Connor <Efithor@gmail.com>
2017-11-06 18:18:45 -0600

    Adding Schema

---
+++ sec/app/models/user.js
@@ -0,0 +1,28 @@
+// app/models/user.js
+// load the things!
+var mongoose = require('mongoose');
+var bcrypt   = require('bcrypt-nodejs');
+
+//define the schema for the user model
+var userSchema = mongoose.Schema({
+
+  local       : {
+    email     : String,
+    password  : String,
+  }
+
+});
+
+// Methods
+// Generating a hash
+userSchema.methods.generateHash = function(password) {
+  return bcrypt.hashSync(password, bycrypt.genSaltSync(8), null);
+};
+
+//Checking if valid
+userSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.local.password);
+};
+
+// create the model for users and expose it.
+module.exports = mongoose.model('User', userSchema0);
